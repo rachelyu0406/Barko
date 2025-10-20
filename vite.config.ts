4e87c1a38aaef28b092080c2c949fdd0159be5f1
mcojon23 <mcojon@icloud.com>
2025-10-20 15:28:34 -0400

    vercel deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,14 @@
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   optimizeDeps: {
+//     exclude: ['lucide-react'],
+//   },
+// });
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
@@ -6,5 +17,10 @@ export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
+  },
+  build: {
+    rollupOptions: {
+      external: ['openai'],
+    },
   },
 });
